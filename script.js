40163fe52b2ef78328a23e1e1abc6390aa7ea212
sayuri-ss <sayuri.20250081@iit.ac.lk>
2025-08-08 10:21:46 +0530

    Add particle background effect to home section and adjust styles for improved aesthetics

--- script.js
+++ script.js
@@ -178,3 +178,109 @@ document.querySelectorAll('.contact-btn, .resume-btn, .send-btn').forEach(button
         }, 1000);
     });
 });
+/* Add this code to the end of your existing script.js file */
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 50, /* Reduced from 80 to 40 */
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#118AB2"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#118AB2",
+            "opacity": 0.2, /* Reduced from 0.4 to 0.2 */
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 3, /* Reduced from 6 to 3 for a calmer movement */
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
